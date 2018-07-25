6801ac98320e2b282c0e331df9f84ddf6ca8fd42
mistercmd <carson.deru@gmail.com>
2018-07-25 09:33:50 -0600

    built library and schedule components and put them on the grid

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import Library from './library/library';
+import Schedule from './schedule/schedule';
 
-class Home extends Component {
+export default class Home extends Component {
   render() {
     return (
       <div className="home">
-        <h1>Course Scheduler</h1>
+        <Library/>
+        <Schedule/>
       </div>
     );
   }
 }
-
-export default Home;

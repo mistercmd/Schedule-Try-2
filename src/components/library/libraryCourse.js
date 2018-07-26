5efca8d721c33080cb90d9e4d49ef78e2000987b
mistercmd <carson.deru@gmail.com>
2018-07-26 10:40:14 -0600

    finishing styling, created action creator

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import Icon from '../icon';
+import Arrow from '../arrow';
+import Action from '../action';
 
 
 class LibraryCourse extends Component {
    render() {
       return (
          <div className="library-course">
-            <label className="library-course__title">Problem Solving</label>
-            { Icon('fas fa-check', 'library-course__icon') }
-         
+            <div className="library-course__title-check">
+                <label className="library-course__title">Problem Solving</label>
+                { Icon('fas fa-check', 'library-course__icon') }
+            </div>
+            
+            <Arrow className="library-course__arrow"/>
 
-            { Icon('fas fa-plus-circle', 'library-course__action')}
+            <Action className="library-course__action"/>
             <div className="library-course__description">
                 <label>Course Description</label>
                 <p>Lorem ipsum dolor sit amet...</p>
--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Gradient from '../gradient';
+import ScheduleCourse from './scheduleCourse';
+import ProgressTracker from './progressTracker';
 
 
 class Schedule extends Component {
@@ -7,6 +9,10 @@ class Schedule extends Component {
       return (
          <div className="schedule">
             <h1 className="schedule__title">My Schedule</h1>
+            <ScheduleCourse title="Problem Solving"/>
+            <ScheduleCourse title="Problem Solving"/>
+            <ScheduleCourse title="Problem Solving"/>
+            <ProgressTracker/>
             <Gradient/>
          </div>
       )

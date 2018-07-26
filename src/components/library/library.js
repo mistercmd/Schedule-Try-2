7bf8064cedd596fa7589c3f8a85333a1f8cfa055
mistercmd <carson.deru@gmail.com>
2018-07-26 10:55:56 -0600

    built courses reducer and connected library component to redux

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../actions';
 import LibraryCourse from './libraryCourse';
 
 
 class Library extends Component {
+
+
+    componentDidMount() {
+        this.props.fetchCourses()
+    }
    render() {
+       
       return (
          <div className="library">
             <h1 className="library__title">Course Library</h1>
@@ -17,4 +26,4 @@ class Library extends Component {
 }
 
 
-export default Library;
+export default connect(null, actions)(Library);
